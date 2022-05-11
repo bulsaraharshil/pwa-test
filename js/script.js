0b943097c4e048bdfb918cf984d6ce9fb6684161
harshil <bulsaraharshil@gmail.com>
2022-05-11 19:40:33 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+if ('serviceWorker' in  navigator){
+    navigator.serviceWorker.register('service-worker.js')
+}
+
+// let btn = document.getElementById('myButton');
+// btn.addEventListener('click', function (){
+//     alert('ok 1');
+// });
