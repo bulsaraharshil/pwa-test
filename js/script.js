646de211b93fc06d840313dd556ca252faccdb2f
bulsaraharshil <bulsaraharshil@gmail.com>
2022-05-18 16:26:11 -0400

    changes in JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 if ('serviceWorker' in  navigator){
     navigator.serviceWorker.register('../service-worker.js ', {scope: '/pwa-test/'})
+    .then(function(){
+        console.log("SW ok");
+    })
 } 
 
 // let btn = document.getElementById('myButton');
