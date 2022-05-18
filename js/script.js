9a91c3aa92c5a91c14da67a65b32a8563205ee52
bulsaraharshil <bulsaraharshil@gmail.com>
2022-05-18 16:17:00 -0400

    style: icons added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in  navigator){
-    navigator.serviceWorker.register('service-worker.js')
-}
+    navigator.serviceWorker.register('../service-worker.js ', {scope: '/pwa-test/'})
+} 
 
 // let btn = document.getElementById('myButton');
 // btn.addEventListener('click', function (){
